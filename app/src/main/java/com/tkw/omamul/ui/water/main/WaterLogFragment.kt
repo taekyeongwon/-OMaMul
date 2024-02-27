11f0edac7752779b4a84d377cc8e8879732cbbd5
taekyeongwon <tkw3351@gmail.com>
2024-02-27 22:42:26 +0900

    ui 준비 - MPAndroidChart 라이브러리 차트 출력 테스트

--- app/src/main/java/com/tkw/omamul/ui/water/main/WaterLogFragment.kt
+++ app/src/main/java/com/tkw/omamul/ui/water/main/WaterLogFragment.kt
@@ -1,19 +1,54 @@
 package com.tkw.omamul.ui.water.main
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.tkw.omamul.R
+import com.tkw.omamul.common.ViewModelFactory
+import com.tkw.omamul.data.model.CupEntity
 import com.tkw.omamul.databinding.FragmentWaterLogBinding
+import com.tkw.omamul.ui.base.BaseFragment
 
-class WaterLogFragment: Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return FragmentWaterLogBinding.inflate(layoutInflater).root
+class WaterLogFragment: BaseFragment<FragmentWaterLogBinding, WaterViewModel>(R.layout.fragment_water_log) {
+    override val viewModel: WaterViewModel by viewModels { ViewModelFactory }
+
+    override fun initView() {
+        val list = ArrayList<BarEntry>()
+        list.add(BarEntry(0f, 100f))
+        list.add(BarEntry(2f, 200f))
+        list.add(BarEntry(4f, 300f))
+        list.add(BarEntry(6f, 400f))
+
+
+        val barDataSet = BarDataSet(list, "").apply {
+            color = ColorTemplate.getHoloBlue()
+            valueTextColor = Color.BLACK
+            valueTextSize = 16f
+        }
+
+        val barData = BarData(barDataSet)
+        dataBinding.barChart.apply {
+            data = barData
+        }
+    }
+
+    override fun bindViewModel(binder: FragmentWaterLogBinding) {
+
+    }
+
+    override fun initObserver() {
+
+    }
+
+    override fun initListener() {
+
     }
 }
