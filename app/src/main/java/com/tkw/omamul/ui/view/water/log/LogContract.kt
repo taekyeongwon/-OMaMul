20b379b0ba2b8f3447812046e39e5d8a7254697f
taekyeongwon <tkw3351@gmail.com>
2024-04-05 22:37:46 +0900

    로그화면 뷰모델 MVI 적용

--- app/src/main/java/com/tkw/omamul/ui/view/water/log/LogContract.kt
+++ app/src/main/java/com/tkw/omamul/ui/view/water/log/LogContract.kt
@@ -3,18 +3,32 @@ package com.tkw.omamul.ui.view.water.log
 import com.tkw.omamul.base.IEvent
 import com.tkw.omamul.base.ISideEffect
 import com.tkw.omamul.base.IState
+import com.tkw.omamul.data.model.DayOfWater
+import com.tkw.omamul.data.model.DayOfWaterList
+import com.tkw.omamul.data.model.Water
 import com.tkw.omamul.ui.view.init.InitContract
 
 class LogContract {
     sealed class Event: IEvent {
-
+        class GetDayAmount(val move: Move): Event()
+        class GetWeekAmount(val move: Move): Event()
+        class GetMonthAmount(val move: Move): Event()
+        object AddDayAmount: Event()
+        object EditDayAmount: Event()
+        class RemoveDayAmount(val water: Water): Event()
     }
 
     sealed class SideEffect: ISideEffect {
-
+        class ShowEditDialog(val isEdit: Boolean): SideEffect()
     }
 
     sealed class State: IState {
         data class Loading(val flag: Boolean): State()
+        data class Complete(val list: DayOfWaterList): State()
+        object Error: State()
+    }
+
+    enum class Move {
+        LEFT, RIGHT, INIT
     }
 }
--- app/src/main/java/com/tkw/omamul/ui/view/water/log/LogViewModel.kt
+++ app/src/main/java/com/tkw/omamul/ui/view/water/log/LogViewModel.kt
@@ -1,17 +1,121 @@
 package com.tkw.omamul.ui.view.water.log
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.tkw.omamul.base.IntentBaseViewModel
+import com.tkw.omamul.common.util.DateTimeUtils
 import com.tkw.omamul.data.WaterRepository
+import com.tkw.omamul.data.model.DayOfWater
+import com.tkw.omamul.data.model.DayOfWaterList
+import com.tkw.omamul.data.model.Water
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class LogViewModel(
     private val waterRepository: WaterRepository
 ): IntentBaseViewModel
 <LogContract.Event, LogContract.State, LogContract.SideEffect>() {
+
+    //현재 날짜
+    private val dateStringFlow = MutableStateFlow(DateTimeUtils.getTodayDate())
+    val dateLiveData = dateStringFlow.asLiveData()
+
+    //전체 DayOfWater 리스트
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val allDayOfWaterLiveData: StateFlow<DayOfWaterList> =
+        waterRepository.getAllDayEntity().mapLatest { list ->
+            DayOfWaterList(
+                list.map { it.toMap() }
+            )
+        }.stateIn(
+            initialValue = DayOfWaterList(arrayListOf()),
+            started = SharingStarted.Eagerly,
+            scope = viewModelScope
+        )
+
+    private val currentDayOfWaterList: List<DayOfWater> = allDayOfWaterLiveData.value.list
+
     override fun createInitialState(): LogContract.State {
         return LogContract.State.Loading(false)
     }
 
     override fun handleEvent(event: LogContract.Event) {
+        when(event) {
+            is LogContract.Event.GetDayAmount -> getDayAmount(event.move)
+            is LogContract.Event.GetWeekAmount -> getWeekAmount(event.move)
+            is LogContract.Event.GetMonthAmount -> getMonthAmount(event.move)
+            LogContract.Event.AddDayAmount -> addDayAmount()
+            is LogContract.Event.EditDayAmount -> editDayAmount()
+            is LogContract.Event.RemoveDayAmount -> removeDayAmount(event.water)
+        }
+    }
 
+    private fun getDayAmount(move: LogContract.Move) {
+        viewModelScope.launch {
+            when(move) {
+                LogContract.Move.LEFT -> {
+                    val currentIndex = getCurrentDateWaterIndex()
+                    if(currentIndex > 0) {
+                        //현재 세팅된 날짜 이전의 날짜 값으로 세팅
+                        dateStringFlow.value = currentDayOfWaterList[currentIndex - 1].date
+                        setEvent(LogContract.Event.GetDayAmount(LogContract.Move.INIT))
+                    }
+                }
+                LogContract.Move.RIGHT -> {
+                    val currentIndex = getCurrentDateWaterIndex()
+                    if(currentIndex < currentDayOfWaterList.size - 1) {
+                        //현재 세팅된 날짜보다 앞의 날짜 값으로 세팅
+                        dateStringFlow.value = currentDayOfWaterList[currentIndex - 1].date
+                        setEvent(LogContract.Event.GetDayAmount(LogContract.Move.INIT))
+                    }
+                }
+                LogContract.Move.INIT -> {
+                    val dayOfWater = waterRepository.getAmount(dateStringFlow.value)?.toMap()
+                    if(dayOfWater != null) {
+                        val list = DayOfWaterList(arrayListOf(dayOfWater))
+                        setState { LogContract.State.Complete(list) }
+                    } else {
+                        setState { LogContract.State.Error }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getWeekAmount(move: LogContract.Move) {
+
+    }
+
+    private fun getMonthAmount(move: LogContract.Move) {
+
+    }
+
+    private fun addDayAmount() {
+        setSideEffect { LogContract.SideEffect.ShowEditDialog(false) }
+    }
+
+    private fun editDayAmount() {
+        setSideEffect { LogContract.SideEffect.ShowEditDialog(true) }
+    }
+
+    private fun removeDayAmount(water: Water) {
+        viewModelScope.launch {
+            waterRepository.deleteAmount(dateStringFlow.value, water.dateTime)
+        }
+    }
+
+    //List<DayOfWater>에서 현재 세팅된 날짜랑 같은 마지막 DayOfWater의 인덱스
+    private fun getCurrentDateWaterIndex(): Int {
+        return allDayOfWaterLiveData.value.list
+            .indexOfLast { it.date == dateStringFlow.value }
     }
 }
