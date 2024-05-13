d4b9086b5c17a9b63f9e020a15286199107dd768
taekyeongwon <tkw3351@gmail.com>
2024-05-14 01:07:09 +0900

    ui모듈 hilt 의존성 제거

--- core/ui/src/main/java/com/tkw/ui/chart/CustomLineChart.kt
+++ core/ui/src/main/java/com/tkw/ui/chart/CustomLineChart.kt
@@ -3,24 +3,13 @@ package com.tkw.ui.chart
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.LimitLine
-import com.github.mikephil.charting.components.MarkerView
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.tkw.ui.chart.base.BaseChart
 import com.tkw.ui.chart.base.MPLineChartBase
-import com.tkw.ui.chart.formatter.XAxisUnitFormatter
-import com.tkw.ui.chart.formatter.XAxisValueFormatter
 import com.tkw.ui.chart.marker.MarkerType
-import com.tkw.ui.chart.renderer.CustomXAxisRenderer
-import com.tkw.ui.chart.renderer.CustomYAxisRenderer
-import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 class CustomLineChart
 @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0)
