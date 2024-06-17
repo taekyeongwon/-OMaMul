3c12826a8c85d0dd2432328c80f90e7e827b7f26
taekyeongwon <tkw3351@gmail.com>
2024-06-18 00:06:14 +0900

    NotificationManagerCompat -> NotifcationManager로 변경.  -> 하위호환성 고려 안하므로

--- core/common/src/main/java/com/tkw/common/WaterAlarmReceiver.kt
+++ core/common/src/main/java/com/tkw/common/WaterAlarmReceiver.kt
@@ -1,17 +1,15 @@
 package com.tkw.common
 
-import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Application
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class WaterAlarmReceiver: BroadcastReceiver() {
+//    @SuppressLint("MissingPermission")
     override fun onReceive(context: Context?, intent: Intent?) {
         if(context != null) {
             val builder = NotificationManager.buildNotification(
@@ -26,37 +24,16 @@ class WaterAlarmReceiver: BroadcastReceiver() {
             )
 
             val notificationId = "${System.currentTimeMillis()}".hashCode()
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                checkPermission(context, notificationId, builder, summaryBuilder)
-            } else {
-                with(NotificationManagerCompat.from(context)) {
-                    notify(notificationId, builder.build())
-                    notify(0, summaryBuilder.build())
-                }
-            }
-//            val notificationManager: android.app.NotificationManager =
-//                context.getSystemService(Application.NOTIFICATION_SERVICE) as android.app.NotificationManager
-//            notificationManager.notify(notificationId, builder.build()) 이걸로 했을 때 권한 해제하면?
+//            with(NotificationManagerCompat.from(context)) {
+//                notify(notificationId, builder.build())
+//                notify(0, summaryBuilder.build())
+//            }
+            val notificationManager: android.app.NotificationManager =
+                context.getSystemService(Application.NOTIFICATION_SERVICE) as android.app.NotificationManager
+            notificationManager.notify(notificationId, builder.build())
+            notificationManager.notify(0, summaryBuilder.build())
         }
 
         Log.d("alarm receiver", "received")
-    }
-
-    private fun checkPermission(
-        context: Context,
-        notificationId: Int,
-        builder: NotificationCompat.Builder,
-        summaryBuilder: NotificationCompat.Builder
-    ) {
-        if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            with(NotificationManagerCompat.from(context)) {
-                notify(notificationId, builder.build())
-                notify(0, summaryBuilder.build())
-            }
-        }
     }
 }
