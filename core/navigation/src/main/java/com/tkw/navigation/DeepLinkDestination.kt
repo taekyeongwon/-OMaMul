f7f90d9a5e5d2f2435ff94931e2345f268e86145
taekyeongwon <tkw3351@gmail.com>
2024-08-17 15:46:23 +0900

    Setting화면 버튼 클릭 시 화면 이동, 다이얼로그 이벤트 추가

--- core/navigation/src/main/java/com/tkw/navigation/DeepLinkDestination.kt
+++ core/navigation/src/main/java/com/tkw/navigation/DeepLinkDestination.kt
@@ -5,6 +5,7 @@ import android.content.Context
 sealed class DeepLinkDestination(val addressRes: Int) {
     object Home: DeepLinkDestination(R.string.home_deeplink)
     object Cup: DeepLinkDestination(R.string.cup_manage_deeplink)
+    object Alarm: DeepLinkDestination(R.string.alarm_deeplink)
 }
 
 fun DeepLinkDestination.getDeepLink(context: Context) = context.getString(this.addressRes)
--- feature-water/setting/src/main/java/com/tkw/setting/WaterSettingFragment.kt
+++ feature-water/setting/src/main/java/com/tkw/setting/WaterSettingFragment.kt
@@ -1,12 +1,17 @@
 package com.tkw.setting
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.tkw.common.autoCleared
+import com.tkw.home.dialog.WaterIntakeDialog
+import com.tkw.navigation.DeepLinkDestination
+import com.tkw.navigation.deepLinkNavigateTo
 import com.tkw.setting.databinding.FragmentSettingBinding
 
 class WaterSettingFragment: Fragment() {
@@ -25,6 +30,7 @@ class WaterSettingFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+        initListener()
     }
 
     private fun initView() {
@@ -32,6 +38,27 @@ class WaterSettingFragment: Fragment() {
             lifecycleOwner = viewLifecycleOwner
             viewModel = this@WaterSettingFragment.viewModel
             executePendingBindings()
+        }
+    }
+
+    private fun initListener() {
+        dataBinding.settingWater.clWaterSettingIntake.setOnClickListener {
+            val dialog = WaterIntakeDialog()
+            dialog.show(childFragmentManager, dialog.tag)
+        }
+        dataBinding.settingWater.clWaterSettingCup.setOnClickListener {
+            findNavController().deepLinkNavigateTo(requireContext(), DeepLinkDestination.Cup)
+        }
+        dataBinding.settingWater.clWaterSettingUnit.setOnClickListener {
+
+        }
+
+        dataBinding.settingAlarm.clAlarmSetting.setOnClickListener {
+            findNavController().deepLinkNavigateTo(requireContext(), DeepLinkDestination.Alarm)
+        }
+
+        dataBinding.settingEtc.clEtcSettingLanguage.setOnClickListener {
+
         }
     }
 }
